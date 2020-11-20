8db83c47847c9799fce0eb41075ef8cbe2b6bdab
techspeterson <techspeterson@gmail.com>
2020-11-21 09:18:55 +1100

    loading in character data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+const XIVAPI = require('xivapi-js');
+const xiv = new XIVAPI();
 
 function App() {
+  const [Character, setCharacter] = useState();
+
+  useEffect(() => {
+    xiv.character.get(31212570, { extended: true })
+      .then(res => {
+        console.log(res)
+        setCharacter(res.Character)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    Character ?
+      <div>
+        <h1>{Character.Name}</h1>
+      </div>
+      : "loading..."
+  )
 }
 
 export default App;
